72e20072df8da8d0f427f32b17ec8e960a9cf165
Jordan O'Byrne <8998564+jnobyrne@users.noreply.github.com>
2024-04-24 12:11:02 -0500

    added new models

--- kuramoto/kuramoto.py
+++ kuramoto/kuramoto.py
@@ -4,10 +4,13 @@ from scipy.integrate import odeint
 
 class Kuramoto:
 
-    def __init__(self, coupling=1, dt=0.01, T=10, n_nodes=None, natfreqs=None):
+    def __init__(self, coupling=1, model='kuramoto', dt=0.01, T=10, n_nodes=None, 
+                 natfreqs=None, coupling2=None, offset=0, noise_strength=0):
         '''
-        coupling: float
+        coupling: float or 2D ndarray
             Coupling strength. Default = 1. Typical values range between 0.4-2
+        model : str
+            Selects the model type. 'kuramoto', 'hmm', 'hmm_modified', 'ghkb' or 'experimental'
         dt: float
             Delta t for integration of equations.
         T: float
@@ -23,6 +26,12 @@ class Kuramoto:
             for the object instance.
             Must be specified if n_nodes is not given.
             If given, it overrides the n_nodes argument.
+        coupling2 : float, optional
+            Coupling strength for the second harmonic in the HMM model
+        offset : float, optional
+            Dephasing offset for the HMM model.
+        noise_strength : float, optional
+            Standard deviation of the Gaussian noise term.
         '''
         if n_nodes is None and natfreqs is None:
             raise ValueError("n_nodes or natfreqs must be specified")
@@ -30,6 +39,10 @@ class Kuramoto:
         self.dt = dt
         self.T = T
         self.coupling = coupling
+        self.model = model
+        self.coupling2 = coupling2
+        self.offset = offset
+        self.noise_strength = noise_strength
 
         if natfreqs is not None:
             self.natfreqs = natfreqs
@@ -44,7 +57,7 @@ class Kuramoto:
         '''
         return 2 * np.pi * np.random.random(size=self.n_nodes)
 
-    def derivative(self, angles_vec, t, adj_mat, coupling):
+    def derivative(self, angles_vec, t, adj_mat, coupling, model, coupling2, offset, noise_strength):
         '''
         Compute derivative of all nodes for current state, defined as
 
@@ -58,9 +71,57 @@ class Kuramoto:
             'Input dimensions do not match, check lengths'
 
         angles_i, angles_j = np.meshgrid(angles_vec, angles_vec)
-        interactions = adj_mat * np.sin(angles_j - angles_i)  # Aij * sin(j-i)
-
-        dxdt = self.natfreqs + coupling * interactions.sum(axis=0)  # sum over incoming interactions
+        if model == 'kuramoto':
+            interactions = adj_mat * np.sin(angles_j - angles_i) # Aij * sin(j-i)
+            dxdt = self.natfreqs + (coupling * interactions).sum(axis=0)  # sum over incoming interactions
+        elif model == 'hmm':
+            rel_phase = angles_j - angles_i
+            interactions = adj_mat * coupling * ( -np.sin(rel_phase + offset) 
+                            + coupling2 * np.sin(2 * rel_phase) )
+            dxdt = self.natfreqs + (interactions).sum(axis=0)  # sum over incoming interactions
+        elif model == 'hmm2':
+            rel_phase = angles_j - angles_i
+            interactions = ( adj_mat * coupling * -np.sin(rel_phase + offset) 
+                            + adj_mat * coupling2 * np.sin(2 * rel_phase) )
+            dxdt = self.natfreqs + (interactions).sum(axis=0)  # sum over incoming interactions
+        elif model == 'hmm_mod1':
+            positive_coupling = np.zeros(np.shape(adj_mat))
+            positive_coupling[coupling > 0] = coupling[coupling > 0]
+            negative_coupling = np.zeros(np.shape(adj_mat))
+            negative_coupling[coupling < 0] = np.abs(coupling[coupling < 0])
+            rel_phase = angles_j - angles_i
+            dxdt = ( self.natfreqs - (positive_coupling * adj_mat * np.sin(rel_phase - offset)).sum(axis=0)
+                    + (negative_coupling * adj_mat * np.sin(2 * rel_phase)).sum(axis=0) )
+        elif model == 'hmm_mod2':
+            # no phase transition between -10,10 ; -10,10 and coupling2= -3
+            rel_phase = angles_j - angles_i
+            interactions = ( adj_mat * coupling2 * (-np.sin(rel_phase) 
+                                 + coupling * np.sin(2 * rel_phase)) )
+            dxdt = self.natfreqs + (interactions).sum(axis=0)  # sum over incoming interactions
+        elif model == 'hmm_mod3':
+            # same as mod1 except for a plus instead of a minus for the first interaction term
+            noise = np.random.normal(0, noise_strength, adj_mat.shape[0])
+            positive_coupling = np.zeros(np.shape(adj_mat))
+            positive_coupling[coupling > 0] = coupling[coupling > 0]
+            negative_coupling = np.zeros(np.shape(adj_mat))
+            negative_coupling[coupling < 0] = np.abs(coupling[coupling < 0])
+            rel_phase = angles_j - angles_i
+            dxdt = ( self.natfreqs + (positive_coupling * adj_mat * np.sin(rel_phase - offset)).sum(axis=0)
+                    + (negative_coupling * adj_mat * np.sin(2 * rel_phase)).sum(axis=0) ) + noise
+        elif model == 'hmm_mod4':
+            rel_phase = angles_j - angles_i
+            interactions = adj_mat * (-np.sin(rel_phase + coupling) + coupling2 * np.sin(2 * rel_phase))
+            dxdt = self.natfreqs + interactions.sum(axis=0)
+        elif model == 'ghkb':
+            rel_phase = angles_j - angles_i
+            interactions = ( adj_mat * coupling * np.sin(rel_phase) 
+                            - adj_mat * coupling2 * np.sin(2 * rel_phase) )
+            dxdt = self.natfreqs + (interactions).sum(axis=0)  # sum over incoming interactions
+        elif model == 'experimental':
+            offsets = np.ones(np.shape(adj_mat)) * np.pi/2
+            offsets[coupling < 0] = 3/2 * np.pi
+            interactions = adj_mat * (np.sin(angles_j - angles_i - offsets) + 1 ) / 2
+            dxdt = self.natfreqs + (np.abs(coupling) * interactions).sum(axis=0)  # sum over incoming interactions
         return dxdt
 
     def integrate(self, angles_vec, adj_mat):
@@ -69,9 +130,13 @@ class Kuramoto:
         # Compute it only once here and pass it to the derivative function
         n_interactions = (adj_mat != 0).sum(axis=0)  # number of incoming interactions
         coupling = self.coupling / n_interactions  # normalize coupling by number of interactions
+        model = self.model
+        coupling2 = self.coupling2
+        offset = self.offset
+        noise_strength = self.noise_strength
 
         t = np.linspace(0, self.T, int(self.T/self.dt))
-        timeseries = odeint(self.derivative, angles_vec, t, args=(adj_mat, coupling))
+        timeseries = odeint(self.derivative, angles_vec, t, args=(adj_mat, coupling, model, coupling2, offset, noise_strength))
         return timeseries.T  # transpose for consistency (act_mat:node vs time)
 
     def run(self, adj_mat=None, angles_vec=None):
@@ -118,4 +183,5 @@ class Kuramoto:
         # Average across complete time window - mean angular velocity (freq.)
         meanfreq = integral / self.T
         return meanfreq
+
 
